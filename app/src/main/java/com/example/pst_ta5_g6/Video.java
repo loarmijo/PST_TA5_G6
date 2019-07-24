ac0b5e740cd3e5b3fae97b135cf9c8486682645d
CesarColoma <ccoloma@espol.edu.ec>
2019-07-23 21:15:04 -0500

    Documentacion del codigo

--- app/src/main/java/com/example/pst_ta5_g6/Video.java
+++ app/src/main/java/com/example/pst_ta5_g6/Video.java
@@ -10,6 +10,7 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 public class Video extends YouTubeBaseActivity {
+    //Se crean los objetos relacionados al View
     YouTubePlayerView YP;
     YouTubePlayer.OnInitializedListener YPlistener;
     Button Play;
@@ -17,29 +18,31 @@ public class Video extends YouTubeBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        //Se relacionan con el archivo xml
         YP= (YouTubePlayerView) findViewById(R.id.youtubeplayer);
         Play=(Button) findViewById(R.id.btnPlay);
+        // Se crea el listener para el reproductor de youtube
         YPlistener= new YouTubePlayer.OnInitializedListener() {
-
-
-
+        //En caso de ser valido se recarga el video escogido previamente
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo("NT8I8ci74qo");
+                youTubePlayer.loadVideo("DiItGE3eAyQ");
             }
-
             @Override
             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
 
             }
         };
+        //Se realiza el evento cuando se da click en el boton
         Play.setOnClickListener(new View.OnClickListener() {
             @Override
+            //Se reproduce el video
             public void onClick(View view) {
                 YP.initialize(Youtube.getApikey(),YPlistener);
             }
         });
     }
+    //Se crea la funcion volver para regresar al menu principal
     public void volver(View view){
         finish();
     }
--- app/src/main/java/com/example/pst_ta5_g6/Youtube.java
+++ app/src/main/java/com/example/pst_ta5_g6/Youtube.java
@@ -1,10 +1,10 @@
 package com.example.pst_ta5_g6;
-
+//Esta clase solo contiene el APIKEY necesario para la seccion del video
 class Youtube {
     public Youtube() {
     }
     private static final String Apikey="AIzaSyAl3Ye5pPJZLWaF7nR85Ri2PHs0awKAb90";
-
+//Metodo getter para obtener el unico atributo de esta clase
     public static String getApikey() {
         return Apikey;
     }
